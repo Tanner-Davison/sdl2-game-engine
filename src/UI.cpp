a103f8637958e8e47b0f635843f2a6a41fbf94a0
Tanner Davison <tanner.davison95@gmail.com>
2025-09-28 23:25:36 -0600

    back to simple UI  manager

--- src/UI.cpp
+++ src/UI.cpp
@@ -1,37 +1,13 @@
 #include "UI.hpp"
-UI::UI(int rowsize, int columnsize) {
-    int RowCount{rowsize}, ColCount{columnsize};
-    Rectangles.reserve(RowCount * ColCount);
-    for (int Row{0}; Row < RowCount; ++Row) {
-        for (int Col{0}; Col < ColCount; ++Col) {
-            bool useGreen{(Row + Col) % 2 == 0};
-            Rectangles.emplace_back(
-                useGreen ? std::make_unique<GreenRectangle>(SDL_Rect{60 * Col, 60 * Row, 50, 50})
-                         : std::make_unique<Rectangle>(SDL_Rect{60 * Col, 60 * Row, 50, 50}));
-        }
-    }
-}
 
-UI::UI() {
-    int RowCount{5}, ColCount{12};
-    Rectangles.reserve(RowCount * ColCount);
-    for (int Row{0}; Row < RowCount; ++Row) {
-        for (int Col{0}; Col < ColCount; ++Col) {
-            bool useGreen{(Row + Col) % 2 == 0};
-            Rectangles.emplace_back(
-                useGreen ? std::make_unique<GreenRectangle>(SDL_Rect{60 * Col, 60 * Row, 50, 50})
-                         : std::make_unique<Rectangle>(SDL_Rect{60 * Col, 60 * Row, 50, 50}));
-        }
-    }
-}
+UI::UI() {}
+
 void UI::Render(SDL_Surface* Surface) const {
-    for (auto& Rectangle : Rectangles) {
-        Rectangle->Render(Surface);
-    }
+    A.Render(Surface);
+    B.Render(Surface);
 };
 
 void UI::HandleEvent(SDL_Event& E) {
-    for (auto& Rectangle : Rectangles) {
-        Rectangle->HandleEvent(E);
-    }
+    A.HandleEvent(E);
+    B.HandleEvent(E);
 };
--- src/UI.hpp
+++ src/UI.hpp
@@ -7,11 +7,11 @@
 
 class UI {
   public:
-    UI(int rowsize, int columnsize);
     UI();
     void Render(SDL_Surface* Surface) const;
     void HandleEvent(SDL_Event& E);
 
   private:
-    std::vector<std::unique_ptr<Rectangle>> Rectangles{};
+    Rectangle A{SDL_Rect{50, 50, 50, 50}};
+    Rectangle B{SDL_Rect{150, 50, 50, 50}};
 };
