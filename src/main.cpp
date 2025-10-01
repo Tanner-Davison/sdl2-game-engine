7d457ba51487992f320f2ac906746061b620ed77
Tanner Davison <tanner.davison95@gmail.com>
2025-10-01 02:00:58 -0600

    created a point render *star simulator

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,117 @@
-/*Copyright (c) 2025 Tanner Davison. All Rights Reserved.*/
-#include "Image.hpp"
-#include "Rectangle.hpp"
-#include "UI.hpp"
-#include "Window.hpp"
-#include <SDL_filesystem.h>
-#include <SDL_timer.h>
-#ifdef __linux__
-    #include <SDL2/SDL.h>
-#else
-    #include <SDL.h>
-#endif
-#include <iostream>
+#include <SDL2/SDL.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(int argc, char** argv) {
-    SDL_Init(SDL_INIT_VIDEO);
-    Window    GameWindow;
-    UI        UIManager;
-    Image     ExampleImg{"game_assets/example.bmp",
-                     GameWindow.GetSurface()->format}; // image blitting
-    SDL_Event E;
-    Uint64    frequency = SDL_GetPerformanceFrequency();
-    while (true) {
-        // 1. Process Events
-        while (SDL_PollEvent(&E)) {
-            /// Process Events
-            UIManager.HandleEvent(E);
-            if (E.type == SDL_QUIT) {
-                SDL_Quit();
-                return 0;
+#define WINDOW_WIDTH 2400
+#define WINDOW_HEIGHT 1800
+#define NUM_POINTS 1000
+#define MIN_PIXELS_PER_SECOND 30
+#define MAX_PIXELS_PER_SECOND 60
+#define POINT_SIZE 3 // Size of each point in pixels
+
+static SDL_Window*   window   = NULL;
+static SDL_Renderer* renderer = NULL;
+static SDL_FPoint    points[NUM_POINTS];
+static float         point_speeds[NUM_POINTS];
+
+float randf() {
+    return (float)rand() / (float)RAND_MAX;
+}
+
+int main(int argc, char* argv[]) {
+    srand(time(NULL));
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
+
+    window = SDL_CreateWindow("SDL2 Points Example",
+                              SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              WINDOW_WIDTH,
+                              WINDOW_HEIGHT,
+                              SDL_WINDOW_SHOWN);
+    if (!window) {
+        SDL_Log("Couldn't create window: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        SDL_Log("Couldn't create renderer: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Initialize points
+    for (int i = 0; i < NUM_POINTS; i++) {
+        points[i].x = randf() * WINDOW_WIDTH;
+        points[i].y = randf() * WINDOW_HEIGHT;
+        point_speeds[i] =
+            MIN_PIXELS_PER_SECOND +
+            (randf() * (MAX_PIXELS_PER_SECOND - MIN_PIXELS_PER_SECOND));
+    }
+
+    Uint64    last_time = SDL_GetTicks64();
+    int       running   = 1;
+    SDL_Event event;
+
+    // Main game loop
+    while (running) {
+        // Handle events
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
             }
-        };
+        }
 
-        /*UNCOMMENT Uint64 PERFOMANCE RENDERING*/
-        // Uint64 Start{SDL_GetPerformanceCounter()};
+        // Calculate delta time
+        Uint64 now     = SDL_GetTicks64();
+        float  elapsed = (now - last_time) / 1000.0f;
+        last_time      = now;
 
-        GameWindow.Render(); /// Render Background Color
+        // Update points
+        for (int i = 0; i < NUM_POINTS; i++) {
+            float distance = elapsed * point_speeds[i];
+            points[i].x += distance;
+            points[i].y += distance;
 
-        ExampleImg.Render(GameWindow.GetSurface());
+            if (points[i].x >= WINDOW_WIDTH || points[i].y >= WINDOW_HEIGHT) {
+                if (rand() % 2) {
+                    points[i].x = randf() * WINDOW_WIDTH;
+                    points[i].y = 0.0f;
+                } else {
+                    points[i].x = 0.0f;
+                    points[i].y = randf() * WINDOW_HEIGHT;
+                }
+                point_speeds[i] =
+                    MIN_PIXELS_PER_SECOND +
+                    (randf() * (MAX_PIXELS_PER_SECOND - MIN_PIXELS_PER_SECOND));
+            }
+        }
 
-        UIManager.Render(GameWindow.GetSurface());
-        GameWindow.Update(); /// Swap Buffers
+        // Render
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-        // 2. Update Objects
-        // 3. Render Changes
+        // Draw points as rectangles
+        for (int i = 0; i < NUM_POINTS; i++) {
+            SDL_FRect rect = {points[i].x - POINT_SIZE / 2,
+                              points[i].y - POINT_SIZE / 2,
+                              POINT_SIZE,
+                              POINT_SIZE};
+            SDL_RenderFillRectF(renderer, &rect);
+        }
 
-        /*UNCOMMENT FOR PERFOMANCE RENDERING*/
-        // Uint64 Delta{SDL_GetPerformanceCounter()};
-        // double elapsedMs = ((Delta - Start) * 1000.0) / frequency;
-        // std::cout << "Render time: " << elapsedMs << " ms" << std::endl;
-    };
+        SDL_RenderPresent(renderer);
+    }
 
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
